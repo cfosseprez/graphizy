b9c1fcee47df72adf5e0b39887b9b20ec852b7f0
janbon <charles.fosseprez@hotmail.fr>
2025-07-18 17:23:13 +0900

    feat: read the doc modification

---
+++ docs/examples/README.md
@@ -0,0 +1,2 @@
+# Examples will be documented in the user guide
+# This directory is kept for potential future example scripts
---
+++ docs/source/conf.py
@@ -0,0 +1,97 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+# Add the project root to the Python path
+sys.path.insert(0, os.path.abspath('../../src'))
+
+# -- Project information -----------------------------------------------------
+project = 'Graphizy'
+copyright = '2025, Charles Fosseprez'
+author = 'Charles Fosseprez'
+release = '0.1.5'
+version = '0.1.5'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx_copybutton',
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = []  # Disable static path to avoid warnings
+
+# -- Extension configuration -------------------------------------------------
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+
+# Autodoc settings
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
+}
+
+# MyST parser settings
+myst_enable_extensions = [
+    "colon_fence",
+]
+
+# Suppress warnings
+suppress_warnings = ['image.not_readable']
+
+# Don't check anchors as external links may be temporary
+linkcheck_ignore = [
+    r'http://localhost:\d+/',
+]
+
+# Theme options
+html_theme_options = {
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 3,
+    'includehidden': True,
+    'titles_only': False
+}
+
+# Show the source link
+html_show_sourcelink = True
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']  # Commented out to avoid warnings
+
+# Custom CSS
+html_css_files = []
+
+# The master toctree document.
+master_doc = 'index'
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'Graphizydoc'
